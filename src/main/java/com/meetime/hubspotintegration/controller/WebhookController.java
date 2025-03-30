9a1caf8d059d85743249ed3911e328b064738a4c
Rodrigo Cavalcante de Barros <rodrigocavalcantedebarros@hotmail.com>
2025-03-30 03:35:24 -0300

    feat: implementar webhook funcional para criação de contatos

--- src/main/java/com/meetime/hubspotintegration/controller/WebhookController.java
+++ src/main/java/com/meetime/hubspotintegration/controller/WebhookController.java
@@ -1,6 +1,5 @@
 package com.meetime.hubspotintegration.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +8,8 @@ import org.springframework.web.bind.annotation.*;
 public class WebhookController {
 
     @PostMapping("/contact")
-    public ResponseEntity<String> receiveContactWebhook(@RequestBody String payload, @RequestHeader("X-HubSpot-Signature") String signature) {
-        System.out.println("Webhook recebido!");
-        System.out.println("Assinatura: " + signature);
-        System.out.println("Payload: " + payload);
-
-        return new ResponseEntity<>("Webhook recebido com sucesso", HttpStatus.OK);
+    public ResponseEntity<String> receiveWebhook(@RequestBody String payload) {
+        System.out.println("Payload recebido:\n" + payload);
+        return ResponseEntity.ok("Recebido com sucesso");
     }
 }
